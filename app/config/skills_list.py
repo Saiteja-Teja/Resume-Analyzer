4573f0652994f59aa76627dced34ce20fa908b1c
SaiTeja Gurram <saitejagurram4@gmail.com>
2026-05-04 18:42:14 +0530

    Add files via upload

---
+++ app/config/skills_list.py
@@ -0,0 +1,10 @@
+skills_list = [
+    "python", "java", "c", "c++", "sql", "mysql", "postgresql", "sql server",
+    "pandas", "numpy", "matplotlib", "seaborn", "scikit-learn", "tensorflow", "pytorch",
+    "machine learning", "deep learning", "nlp", "computer vision",
+    "flask", "fastapi", "streamlit", "django",
+    "html", "css", "javascript",
+    "docker", "kubernetes", "aws", "aws ec2", "git", "github",
+    "power bi", "tableau", "excel",
+    "cnn", "rnn", "ann", "lstm"
+]
